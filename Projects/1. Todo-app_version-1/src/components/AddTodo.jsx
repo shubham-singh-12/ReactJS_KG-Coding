cc023aceee3299eac6b23c49200e377d78a76e76
shubham-singh-12 <shubhamytsingh@gmail.com>
2025-04-13 08:59:57 +0530

    Update from class to className

--- Projects/1. Todo-app_version-1/src/components/AddTodo.jsx
+++ Projects/1. Todo-app_version-1/src/components/AddTodo.jsx
@@ -1,21 +1,21 @@
 function AddTodo() {
 
-  return <div class='container m-2'>
-    <div class='row'>
+  return <div className='container m-2'>
+    <div className='row'>
 
       {/* Todo Input Box */}
-      <div class='col-6'>
+      <div className='col-6'>
         <input type="text" name="" id="" placeholder="Enter Todo Here" />
       </div>
 
       {/* Date Input Box */}
-      <div class='col-4'>
+      <div className='col-4'>
         <input type="date" />
       </div>
 
       {/* Button for submit */}
-      <div class='col-2'>
-        <button type='button' class='btn btn-success kg-button'>Add</button>
+      <div className='col-2'>
+        <button type='button' className='btn btn-success kg-button'>Add</button>
       </div>
     </div>
   </div>
--- Projects/1. Todo-app_version-1/src/components/TodoItem_1.jsx
+++ Projects/1. Todo-app_version-1/src/components/TodoItem_1.jsx
@@ -3,18 +3,18 @@ function TodoItem_1() {
   let todoName = 'Buy Milk';
   let todoDate = '4/10/2024';
 
-  return <div class='container m-2'>
-    <div class='row Kg-row'>
+  return <div className='container m-2'>
+    <div className='row Kg-row'>
 
       {/* Todo Input Box */}
-      <div class='col-6'>{todoName}</div>
+      <div className='col-6'>{todoName}</div>
 
       {/* Date Input Box */}
-      <div class='col-4'>{todoDate}</div>
+      <div className='col-4'>{todoDate}</div>
 
       {/* Button for submit */}
-      <div class='col-2'>
-        <button type='button' class='btn btn-danger kg-button'>Delete</button>
+      <div className='col-2'>
+        <button type='button' className='btn btn-danger kg-button'>Delete</button>
       </div>
     </div>
   </div>
